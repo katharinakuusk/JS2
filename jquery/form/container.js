163107f4a2c67ef7e6c33e56c49a7ab9169feedd
katharinakuusk <katharinakuusk@gmail.com>
2019-01-20 12:42:20 +0300

    solved

--- jquery/form/container.js
+++ jquery/form/container.js
@@ -2,7 +2,7 @@ class Container {
     constructor(html, content, className, parent, id) {
         this.html =  html;
         this.content = content;
-        this.class = className;
+        this.className = className;
         this.id = id;
         this.parent = parent;
     }
@@ -13,16 +13,12 @@ class Container {
     }
 }
 
-class element extends Container {
+class Element extends Container {
     constructor (html, content, className, parent, id) {
         super(html, content, className, parent);
     }
     
     render() {
         $( "<" + this.html + " class='" + this.className + "'>" + this.content + "</" + this.html + ">" ).appendTo(this.parent);
-        /*let $node = "<" + this.html + ">" + this.content + "</" + this.html + ">";
-        $(this.parent).append($node);*/
-        //$node.addClass(".cities__options_option");
-    
     }
 }
